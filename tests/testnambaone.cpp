6ee3282bfc2bd678558b38afa87dec4420888014
DanilAndreev <danssg08@gmail.com>
2021-09-08 20:56:57 +0300

    [DTypes::Table :: Testing] Adding tests.

--- tests/testnambaone.cpp
+++ tests/testnambaone.cpp
@@ -25,11 +25,14 @@
 //
 
 #include <gtest/gtest.h>
+#include <DataTypes/Table.h>
 
 // Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-	// Expect two strings not to be equal.
-	EXPECT_STRNE("hello", "world");
-	// Expect equality.
-	EXPECT_EQ(7 * 6, 42);
+TEST(DTypes_Table, Creation) {
+    DTypes::Table table{};
+
+//	// Expect two strings not to be equal.
+//	EXPECT_STRNE("hello", "world");
+//	// Expect equality.
+//	EXPECT_EQ(7 * 6, 42);
 }
